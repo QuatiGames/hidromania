c634c188ddac3bdb77522915942237f8a74eb446
Lucas Costa <lucascostaa73@gmail.com>
2017-10-17 19:18:52 -0200

    Adding base directory

---
+++ HidroMania/HidroMania/GameLayer.swift
@@ -0,0 +1,37 @@
+//
+//  GameLayer.swift
+//  HidroMania
+//
+//  Created by Lucas Araujo on 17/10/17.
+//  Copyright © 2017 QuatiGames. All rights reserved.
+//
+
+import SpriteKit
+
+class GameLayer:SKSpriteNode {
+    
+    convenience init(size: CGSize){
+        self.init(color: UIColor.clear, size: size)
+    }
+    
+    func update(delta:Double){
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+}
