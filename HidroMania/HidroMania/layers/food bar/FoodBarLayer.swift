b3d63d04005fb7190becc4bd22f6d0961402e2e3
Lucas Costa <lucascostaa73@gmail.com>
2017-10-17 21:39:55 -0200

    Adding igredients sequence

--- HidroMania/HidroMania/layers/food bar/FoodBarLayer.swift
+++ HidroMania/HidroMania/layers/food bar/FoodBarLayer.swift
@@ -10,9 +10,36 @@ import SpriteKit
 
 class FoodBarLayer:Layer {
     
+    var foodArray:Array = Array<Ingredient>()
+    var maxIngredient = 5;
+    
+    
     override func didMove() {
         self.color = UIColor.yellow
         self.colorBlendFactor = 1.0
+        
+        setupFoods()
+    }
+    
+    func setupFoods(){
+        
+        print(self.size)
+        print(self.size.width*0.5)
+        
+        let ratio = self.size.width*0.4
+        let space:CGFloat = 20
+        
+        for i in 1...maxIngredient {
+            let food = Ingredient(color: UIColor.white, size: CGSize(width: ratio, height: ratio) )
+            food.anchorPoint = CGPoint(x: 0.5, y:1.0)
+            
+            food.position.y = -(ratio + space) * CGFloat(i - 1) - ratio
+            food.position.x = self.size.width*0.5
+            
+            foodArray.append(food)
+            self.addChild(food)
+        }
+        
     }
     
 }
---
+++ HidroMania/HidroMania/layers/food bar/Ingredient.swift
@@ -0,0 +1,15 @@
+//
+//  Food.swift
+//  HidroMania
+//
+//  Created by Lucas Araujo on 17/10/17.
+//  Copyright © 2017 QuatiGames. All rights reserved.
+//
+
+import SpriteKit
+
+class Ingredient:SKSpriteNode {
+    
+    
+    
+}
